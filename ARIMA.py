2f6f2407f28b5ce84a91e7e77f2a0c6e45173151
eegokk <eminegok10@gmail.com>
2025-05-17 20:28:20 +0300

    Update ARIMA.py

--- ARIMA.py
+++ ARIMA.py
@@ -5,6 +5,9 @@ This is a temporary script file.
 """
 import cx_Oracle
 import pandas as pd
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.arima.model import ARIMA
 
 # Bağlantı bilgileri
 username = 'ECINAR'  # Veritabanı kullanıcı adınız
@@ -45,37 +48,64 @@ except cx_Oracle.DatabaseError as e:
     print("Veritabanı bağlantı hatası:", e)
     
     
-import pandas as pd
-import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
+
 
 # Zaman serisi verinizi hazırlayın
 df['TARIH'] = pd.to_datetime(df['TARIH'])  # Zaman sütunu varsa
 df.set_index('TARIH', inplace=True)        # TARIH sütununu index olarak ayarla
 
-# Tahmin etmek istediğiniz sütunu belirleyin
-series = df['SAYI']  # Örnek veri sütunu
+# Tahmin etmek istediğiniz sütunu belirleyin sıralı
+series = df['SAYI'].sort_index()
 
 # Grafikle kontrol et
 series.plot(title='Zaman Serisi Verisi')
 plt.show()
 
+
+from pmdarima import auto_arima
+
+stepwise_fit = auto_arima(series, start_p=0, start_q=0,
+                          max_p=5, max_q=5, m=1,
+                          start_P=0, seasonal=False,
+                          d=None, trace=True,
+                          error_action='ignore',  
+                          suppress_warnings=True, 
+                          stepwise=True)
+print('buradan sonrasına bak')
+print(stepwise_fit.summary())
+
+order = stepwise_fit.order
 # ARIMA modelini kur (örnek: ARIMA(1,1,1))
-model = ARIMA(series, order=(1, 1, 1))
+model = ARIMA(series, order=order)
+#model = ARIMA(series, order=(1, 0, 0))
+#model = ARIMA(series, order=(1, 1, 1))
 model_fit = model.fit()
+
+
 
 # Model özetini yazdır
 print(model_fit.summary())
 
 # Gelecek 10 günü tahmin et
-forecast = model_fit.forecast(steps=10)
+#forecast = model_fit.forecast(steps=10)
+#print("Tahmin sonuçları:")
+#print(forecast)
+
+#30 Günlük tahmin 
+forecast = model_fit.forecast(steps=30)
+forecast_index = pd.date_range(start=series.index[-1], periods=31, freq='D')[1:]
 print("Tahmin sonuçları:")
 print(forecast)
 
 # Tahminleri grafikte göster
+plt.figure(figsize=(12,6))
 plt.plot(series, label='Gerçek Veri')
-forecast_index = pd.date_range(start=series.index[-1], periods=11, freq='D')[1:]
 plt.plot(forecast_index, forecast, label='Tahmin', color='red')
-plt.legend()
 plt.title('ARIMA Tahmini')
+plt.xlabel('Tarih')
+plt.ylabel('Geri Dönüş Sayısı')
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
 plt.show()
+
