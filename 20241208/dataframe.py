17ca4400927f891ae6715db472ab7d379a3f0239
eegokk <eminegok10@gmail.com>
2025-05-26 23:01:06 +0300

    Create dataframe.py

---
+++ 20241208/dataframe.py
@@ -0,0 +1,42 @@
+
+
+import cx_Oracle
+import pandas as pd
+
+# Bağlantı bilgileri
+username = 'ECINAR'  # Veritabanı kullanıcı adınız
+password = '123'     # Veritabanı şifreniz
+dsn = '127.0.0.1:1521/orcl'  # Veritabanı bağlantı adresi (localhost, port ve service name)
+
+try:
+    # Oracle veritabanına bağlantı
+    connection = cx_Oracle.connect(username, password, dsn)
+    print("Bağlantı başarılı ✅")
+
+    # Bağlantıyı kontrol etmek için bir sorgu çalıştıralım
+    cursor = connection.cursor()
+    query = "SELECT * FROM ECINAR.YK_GGD_SAYI "
+    cursor.execute(query)
+
+    # Sütun adlarını al
+    columns = [col[0] for col in cursor.description]
+
+    # Verileri al
+    data = cursor.fetchall()
+
+    # DataFrame'e dönüştür
+    df = pd.DataFrame(data, columns=columns)
+
+    # Sonuçları yazdır
+    #print("Veriler DataFrame olarak alındı:")
+    #print(df)
+    print(df.head())  # İlk 5 satır
+    #print(df.columns)         # Sütun isimlerini göster
+    #print(df.iloc[:, :2])  
+
+    # Bağlantıyı kapat
+    cursor.close()
+    connection.close()
+
+except cx_Oracle.DatabaseError as e:
+    print("Veritabanı bağlantı hatası:", e)
