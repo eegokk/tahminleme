41488f49e1467dd5f73fd956a84c107c9fdb4bbb
eegokk <eminegok10@gmail.com>
2025-05-26 22:59:19 +0300

    Create 2017_temp.py

---
+++ 2017_temp.py
@@ -0,0 +1,48 @@
+import cx_Oracle
+import pandas as pd
+
+# Bağlantı bilgileri
+username = 'ECINAR'  
+password = '123'  
+dsn = '127.0.0.1:1521/orcl'  
+
+try:
+    # Oracle veritabanına bağlantı
+    connection = cx_Oracle.connect(username, password, dsn)
+    print("Bağlantı başarılı ✅")
+
+    # Bağlantıyı kontrol etmek için çalıştırılan sorgu
+    cursor = connection.cursor()
+    query = "SELECT * FROM ECINAR.YK_GGD_HAFTALIK "
+    cursor.execute(query)
+
+    # Sütun adlarını al
+    columns = [col[0] for col in cursor.description]
+
+    # Verileri al
+    data = cursor.fetchall()
+
+    # DataFrame'e dönüştür
+    df = pd.DataFrame(data, columns=columns)
+
+    # Sonuçları yazdır
+    #print("Veriler DataFrame olarak alındı:")
+    #print(df)
+    #print(df.head())  
+    print(df.columns)        
+    print(df.iloc[:, :2])  
+
+    # Bağlantıyı kapat
+    cursor.close()
+    connection.close()
+
+except cx_Oracle.DatabaseError as e:
+    print("Veritabanı bağlantı hatası:", e)
+    
+#dataframe oluştur    
+df = pd.DataFrame(data, columns=columns)    
+df['tarih'] = pd.to_datetime(df['TARIH'])
+df.set_index('TARIH', inplace=True)
+df.rename(columns={'SAYI': 'geri_donus_sayisi'}, inplace=True)
+df = df.sort_index()
+
