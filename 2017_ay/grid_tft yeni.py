6d15b73f45c267f927a9b3782ee5f4e51e8cfc4b
eegokk <eminegok10@gmail.com>
2025-05-31 22:25:50 +0300

    Update grid_tft yeni.py

--- 2017_ay/grid_tft yeni.py
+++ 2017_ay/grid_tft yeni.py
@@ -5,24 +5,24 @@ import pandas as pd
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
+import pytorch_lightning as pl
 from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_forecasting import TimeSeriesDataSet, TemporalFusionTransformer, SMAPE
 from pytorch_forecasting.data import GroupNormalizer
-
-# Ortam ayarları
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-warnings.filterwarnings("ignore")
-np.float = float
-
-# Sabit tohumlar
-SEED = 42
 import random
+import gc
+
+SEED = 42
+os.environ["PYTHONHASHSEED"] = str(SEED)
 random.seed(SEED)
-torch.manual_seed(SEED)
 np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
 
 # Veritabanı bağlantısı
 username = 'ECINAR'
@@ -50,84 +50,97 @@ df["time_idx"] = (df["TARIH"] - df["TARIH"].min()).dt.days // 30
 df["AY"] = 0
 
 
-
 # Grid Search Parametreleri
-hidden_sizes = [8, 16, 24,36]
-dropouts = [0.1, 0.2, 0.3]
-epoch_list = [40,50,75]  
+hidden_sizes = [16]
+dropouts = [0.1,0.2]
+epoch_list = [2]
+batch_size = [16,32]
 
 results = []
 
 for hidden_size in hidden_sizes:
     for dropout in dropouts:
         for ep in epoch_list:
-            print(f"\n➡️ Grid Search: hidden_size={hidden_size}, dropout={dropout}, ep={ep}")
+            for bs in batch_size:
+                print(dropout)
+                print(f"\n➡️ Grid Search: hidden_size={hidden_size}, dropout={dropout}, ep={ep}, bs={batch_size}")
 
-            max_encoder_length = 12
-            max_prediction_length = 12
-            training_cutoff = df["time_idx"].max() - max_prediction_length
+                max_encoder_length = 12
+                max_prediction_length = 12
+                training_cutoff = df["time_idx"].max() - max_prediction_length
+                print('time_idx', df.time_idx)
+                print('DF', len(df))
+                print('training_cutoff', training_cutoff)
+                
+                training = TimeSeriesDataSet(
+                    df[df.time_idx <= training_cutoff],
+                    time_idx="time_idx",
+                    target="SAYI",
+                    group_ids=["AY"],
+                    max_encoder_length=max_encoder_length,
+                    max_prediction_length=max_prediction_length,
+                    time_varying_known_reals=["time_idx"],
+                    time_varying_unknown_reals=["SAYI"],
+                    target_normalizer=GroupNormalizer(groups=["AY"]),
+                    allow_missing_timesteps=True)
 
-            training = TimeSeriesDataSet(
-                df[df.time_idx <= training_cutoff],
-                time_idx="time_idx",
-                target="SAYI",
-                group_ids=["AY"],
-                max_encoder_length=max_encoder_length,
-                max_prediction_length=max_prediction_length,
-                time_varying_known_reals=["time_idx"],
-                time_varying_unknown_reals=["SAYI"],
-                target_normalizer=GroupNormalizer(groups=["AY"]),
-                allow_missing_timesteps=True
-            )
+                validation = TimeSeriesDataSet.from_dataset(training, df, predict=True, stop_randomization=True)
+                train_loader = training.to_dataloader(train=True, batch_size=bs, num_workers=0)
+                val_loader = validation.to_dataloader(train=False, batch_size=bs, num_workers=0)
+                
+                print('val_loader', len(val_loader))
+                
+                tft = TemporalFusionTransformer.from_dataset(
+                    training,
+                    learning_rate=0.03,
+                    hidden_size=hidden_size,
+                    attention_head_size=1,
+                    dropout=dropout,
+                    loss=SMAPE(),
+                    log_interval=1,
+                    reduce_on_plateau_patience=4
+                )
 
-            validation = TimeSeriesDataSet.from_dataset(training, df, predict=True, stop_randomization=True)
-            train_loader = training.to_dataloader(train=True, batch_size=16, num_workers=0)
-            val_loader = validation.to_dataloader(train=False, batch_size=16, num_workers=0)
+                early_stop = EarlyStopping(monitor="val_loss", patience=10, mode="min")
+                lr_logger = LearningRateMonitor()
+                trainer = pl.Trainer(
+                    max_epochs=ep,
+                    gpus=1 if torch.cuda.is_available() else 0,
+                    gradient_clip_val=0.1,
+                    callbacks=[early_stop, lr_logger],
+                    logger=TensorBoardLogger("lightning_logs")
+                )
 
-            tft = TemporalFusionTransformer.from_dataset(
-                training,
-                learning_rate=0.03,
-                hidden_size=hidden_size,
-                attention_head_size=1,
-                dropout=dropout,
-                loss=SMAPE(),
-                log_interval=0,
-                reduce_on_plateau_patience=4,
-            )
+                trainer.fit(
+                    model=tft,
+                    train_dataloaders=train_loader,
+                    val_dataloaders=val_loader
+                )
+               
+                val_predictions, x_val = tft.predict(val_loader, mode="prediction", return_x=True)
+                val_predicted = val_predictions[0].detach().cpu().numpy().squeeze()
+                val_true = x_val["decoder_target"][0].detach().cpu().numpy().squeeze()
 
-            early_stop = EarlyStopping(monitor="val_loss", patience=5, mode="min")
-            trainer = Trainer(
-                max_epochs=ep,
-                gpus=1 if torch.cuda.is_available() else 0,
-                gradient_clip_val=0.1,
-                callbacks=[early_stop, LearningRateMonitor()],
-                logger=TensorBoardLogger("logs")
-            )
+                mae = mean_absolute_error(val_true, val_predicted)
+                rmse = mean_squared_error(val_true, val_predicted, squared=False)
+                mape = mean_absolute_percentage_error(val_true, val_predicted)
+                mase = mae / np.mean(np.abs(np.diff(val_true)))
 
-            trainer.fit(tft, train_dataloaders=train_loader, val_dataloaders=val_loader)
-
-            val_predictions, x_val = tft.predict(val_loader, mode="prediction", return_x=True)
-            true = x_val["decoder_target"][0].detach().cpu().numpy()
-            pred = val_predictions[0].detach().cpu().numpy()
-
-            mae = mean_absolute_error(true, pred)
-            rmse = mean_squared_error(true, pred, squared=False)
-            mape = mean_absolute_percentage_error(true, pred)
-            mase = mae / np.mean(np.abs(np.diff(true)))
-
-            results.append({
-                "hidden_size": hidden_size,
-                "dropout": dropout,
-                "ep": ep,
-                "MAE": mae,
-                "RMSE": rmse,
-                "MAPE": mape * 100,
-                "MASE": mase
-            })
-
-
+                results.append({
+                    "hidden_size": hidden_size,
+                    "dropout": dropout,
+                    "ep": ep,
+                    "batch_size": bs,
+                    #"MAE": mae,
+                    "RMSE": rmse,
+                    "MAPE": mape * 100,
+                    "MASE": mase
+                })
+                del tft
+                gc.collect()
+                torch.cuda.empty_cache()
+            
 # Sonuçları yazdır
 results_df = pd.DataFrame(results)
 print("\n📊 Grid Search Sonuçları:")
 print(results_df.sort_values("MASE"))
-
